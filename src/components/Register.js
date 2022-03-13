4f3f21560c120c3f1a1687b0afec0f1468d0ac0b
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-03-13 09:43:16 +0100

    first commit

---
+++ src/components/Register.js
@@ -0,0 +1,93 @@
+import { useRef, useState, useEffect } from "react";
+import { faCheck,faTimes,faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const REGISTER_URL = '/register';
+
+
+const Register = () => {
+
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState("")
+    const [validName, setValidName] = useState(false)
+    const [userFocus, setUserFocus] = useState(false)
+
+
+    const [password, setPassword] = useState("")
+    const [validPassword, setValidPassword] = useState(false)
+    const [passwordFocus, setPasswordFocus] = useState(false)
+
+    const [matchPassword, setMatchPassword] = useState('');
+    const [validMatch, setValidMatch] = useState(false);
+    const [matchFocus, setMatchFocus] = useState(false);
+
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
+
+    useEffect(() => {
+      
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        const result = USER_REGEX.test(user)
+        console.log(result);
+        console.log(user)
+        setValidName(result)
+    }, [user])
+
+    useEffect(() => {
+        const result = PASSWORD_REGEX.test(password)
+        console.log(result);
+        console.log(password)
+        setValidPassword(result)
+        const match = password === matchPassword
+        setValidMatch(match)
+    }, [password, matchPassword])
+
+    useEffect(() => {
+        setErrMsg("");
+
+    },[user, password, matchPassword])
+    
+     
+  return (
+    <section>
+        <p ref={errRef} className={errMsg ? "errmsg" : 
+        "offscreen"} aria-live="assertive">{errMsg}</p>
+        <h1>Register</h1>
+        <form>
+            <label htmlFor="username">
+                Username:    
+            </label>
+            <input
+                type="text"
+                id="username"
+                ref={userRef}
+                autoComplete="off"
+                onChange={(e) => setUser(e.target.value)}
+                value={user}
+                required
+                aria-invalid={validName ? "false" : "true"}
+                aria-describedby="uidnote"
+                onFocus={() => setUserFocus(true)}
+                onBlur={() => setUserFocus(false)}
+            />
+            <p id="uidnote" className={userFocus && user 
+                && !validName ? "instructions" : "offscreen"}>
+                <FontAwesomeIcon icon={faInfoCircle} />
+                4 to 24 characters.<br />
+                Must begin with a letter.<br />
+                Letters, numbers, underscores, hyphens allowed.
+            </p>
+        </form>
+    </section>
+  )
+}
+
+export default Register
